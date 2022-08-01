5a98493168e79e40411f8bafd20463ab17ce55f2
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2022-08-01 18:18:17 +0200

    Add dx functions

--- EQlib.Mkl/HyperJet.cs
+++ EQlib.Mkl/HyperJet.cs
@@ -7,6 +7,320 @@ using static Blas;
 
 public class HyperJet
 {
+	public static void Unary(int size, double f, double da, ReadOnlySpan<double> a, Span<double> r)
+	{
+		Debug.Assert(size >= 0);
+
+		var n = size + 1;
+
+		Debug.Assert(a.Length == n);
+		Debug.Assert(r.Length == n);
+
+		unsafe
+		{
+			fixed (double* aPtr = a)
+			fixed (double* rPtr = r)
+			{
+				*rPtr = f;
+
+				var a1Ptr = aPtr + 1;
+
+				var kPtr = rPtr + 1;
+
+				cblas_daxpy(n - 1, da, a1Ptr, 1, kPtr, 1);
+			}
+		}
+	}
+
+	public static void Binary(int size, double f, double da, double db, ReadOnlySpan<double> a, ReadOnlySpan<double> b, Span<double> r)
+	{
+		Debug.Assert(size >= 0);
+
+		var n = size + 1;
+
+		Debug.Assert(a.Length == n);
+		Debug.Assert(b.Length == n);
+		Debug.Assert(r.Length == n);
+
+		unsafe
+		{
+			fixed (double* aPtr = a)
+			fixed (double* bPtr = b)
+			fixed (double* rPtr = r)
+			{
+				*rPtr = f;
+
+				var a1Ptr = aPtr + 1;
+				var b1Ptr = bPtr + 1;
+
+				var kPtr = rPtr + 1;
+
+				cblas_daxpy(n - 1, da, a1Ptr, 1, kPtr, 1);
+				cblas_daxpy(n - 1, db, b1Ptr, 1, kPtr, 1);
+			}
+		}
+	}
+
+	public static void Ternary(int size, double f, double da, double db, double dc, ReadOnlySpan<double> a, ReadOnlySpan<double> b, ReadOnlySpan<double> c, Span<double> r)
+	{
+		Debug.Assert(size >= 0);
+
+		var n = size + 1;
+
+		Debug.Assert(a.Length == n);
+		Debug.Assert(b.Length == n);
+		Debug.Assert(c.Length == n);
+		Debug.Assert(r.Length == n);
+
+		unsafe
+		{
+			fixed (double* aPtr = a)
+			fixed (double* bPtr = b)
+			fixed (double* cPtr = c)
+			fixed (double* rPtr = r)
+			{
+				*rPtr = f;
+
+				var a1Ptr = aPtr + 1;
+				var b1Ptr = bPtr + 1;
+				var c1Ptr = cPtr + 1;
+
+				var kPtr = rPtr + 1;
+
+				cblas_daxpy(n - 1, da, a1Ptr, 1, kPtr, 1);
+				cblas_daxpy(n - 1, db, b1Ptr, 1, kPtr, 1);
+				cblas_daxpy(n - 1, dc, c1Ptr, 1, kPtr, 1);
+			}
+		}
+	}
+
+
+	public static void DNegative(int size, ReadOnlySpan<double> a, Span<double> r)
+	{
+		Debug.Assert(size >= 0);
+
+		var n = size + 1;
+
+		Debug.Assert(a.Length == n);
+		Debug.Assert(r.Length == n);
+
+		unsafe
+		{
+			fixed (double* aPtr = a)
+			fixed (double* rPtr = r)
+			{
+				cblas_daxpy(n, -1, aPtr, 1, rPtr, 1);
+			}
+		}
+	}
+
+
+	public static void DAdd(int size, double a, ReadOnlySpan<double> b, Span<double> r)
+	{
+		Debug.Assert(size >= 0);
+
+		var n = size + 1;
+
+		Debug.Assert(b.Length == n);
+		Debug.Assert(r.Length == n);
+
+		unsafe
+		{
+			fixed (double* bPtr = b)
+			fixed (double* rPtr = r)
+			{
+				cblas_dcopy(n, bPtr, 1, rPtr, 1);
+				rPtr[0] += a;
+			}
+		}
+	}
+
+	public static void DAdd(int size, ReadOnlySpan<double> a, ReadOnlySpan<double> b, Span<double> r)
+	{
+		Debug.Assert(size >= 0);
+
+		var n = size + 1;
+
+		Debug.Assert(a.Length == n);
+		Debug.Assert(b.Length == n);
+		Debug.Assert(r.Length == n);
+
+		unsafe
+		{
+			fixed (double* aPtr = a)
+			fixed (double* bPtr = b)
+			fixed (double* rPtr = r)
+			{
+				Vml.vdAdd(n, aPtr, bPtr, rPtr);
+			}
+		}
+	}
+
+
+	public static void DSubtract(int size, double a, ReadOnlySpan<double> b, Span<double> r)
+	{
+		Debug.Assert(size >= 0);
+
+		var n = size + 1;
+
+		Debug.Assert(b.Length == n);
+		Debug.Assert(r.Length == n);
+
+		unsafe
+		{
+			fixed (double* bPtr = b)
+			fixed (double* rPtr = r)
+			{
+				cblas_daxpy(n, -1, bPtr, 1, rPtr, 1);
+				rPtr[0] += a;
+			}
+		}
+	}
+
+	public static void DSubtract(int size, ReadOnlySpan<double> a, ReadOnlySpan<double> b, Span<double> r)
+	{
+		Debug.Assert(size >= 0);
+
+		var n = size + 1;
+
+		Debug.Assert(a.Length == n);
+		Debug.Assert(b.Length == n);
+		Debug.Assert(r.Length == n);
+
+		unsafe
+		{
+			fixed (double* aPtr = a)
+			fixed (double* bPtr = b)
+			fixed (double* rPtr = r)
+			{
+				Vml.vdSub(n, aPtr, bPtr, rPtr);
+			}
+		}
+	}
+
+
+	public static void DMultiply(int size, double a, ReadOnlySpan<double> b, Span<double> r)
+	{
+		Debug.Assert(size >= 0);
+
+		var n = size + 1;
+
+		Debug.Assert(b.Length == n);
+		Debug.Assert(r.Length == n);
+
+		unsafe
+		{
+			fixed (double* bPtr = b)
+			fixed (double* rPtr = r)
+			{
+				rPtr[0] = a * bPtr[0];
+
+				var db = a;
+
+				cblas_daxpy(n, db, bPtr, 1, rPtr, 1);
+			}
+		}
+	}
+
+	public static void DMultiply(int size, ReadOnlySpan<double> a, ReadOnlySpan<double> b, Span<double> r)
+	{
+		Debug.Assert(size >= 0);
+
+		var n = size + 1;
+
+		Debug.Assert(a.Length == n);
+		Debug.Assert(b.Length == n);
+		Debug.Assert(r.Length == n);
+
+		unsafe
+		{
+			fixed (double* aPtr = a)
+			fixed (double* bPtr = b)
+			fixed (double* rPtr = r)
+			{
+				rPtr[0] = aPtr[0] * bPtr[0];
+
+				var da = bPtr[0];
+				var db = aPtr[0];
+
+				var a1Ptr = aPtr + 1;
+				var b1Ptr = bPtr + 1;
+
+				var kPtr = rPtr + 1;
+
+				cblas_daxpy(n - 1, da, a1Ptr, 1, kPtr, 1);
+				cblas_daxpy(n - 1, db, b1Ptr, 1, kPtr, 1);
+			}
+		}
+	}
+
+
+	public static void DDivide(int size, ReadOnlySpan<double> a, ReadOnlySpan<double> b, Span<double> r)
+	{
+		Debug.Assert(size >= 0);
+
+		var n = size + 1;
+
+		Debug.Assert(a.Length == n);
+		Debug.Assert(b.Length == n);
+		Debug.Assert(r.Length == n);
+
+		unsafe
+		{
+			fixed (double* aPtr = a)
+			fixed (double* bPtr = b)
+			fixed (double* rPtr = r)
+			{
+				var da = 1 / bPtr[0];
+				var tmp = da * da;
+				var db = -aPtr[0] * tmp;
+				var dadb = -1 * tmp;
+				var dbdb = -2 * db * da;
+
+				rPtr[0] = aPtr[0] / bPtr[0];
+
+				var a1Ptr = aPtr + 1;
+				var b1Ptr = bPtr + 1;
+
+				var kPtr = rPtr + 1;
+
+				cblas_daxpy(n - 1, da, a1Ptr, 1, kPtr, 1);
+				cblas_daxpy(n - 1, db, b1Ptr, 1, kPtr, 1);
+			}
+		}
+	}
+
+	public static void DDivide(int size, double a, ReadOnlySpan<double> b, Span<double> r)
+	{
+		Debug.Assert(size >= 0);
+
+		var n = size + 1;
+
+		Debug.Assert(b.Length == n);
+		Debug.Assert(r.Length == n);
+
+		unsafe
+		{
+			fixed (double* bPtr = b)
+			fixed (double* rPtr = r)
+			{
+				var db = -a / (bPtr[0] * bPtr[0]);
+				var dbdb = 2 * a / (bPtr[0] * bPtr[0] * bPtr[0]);
+
+				rPtr[0] = a / bPtr[0];
+
+				var b1Ptr = bPtr + 1;
+
+				var kPtr = rPtr + 1;
+
+				cblas_daxpy(n - 1, db, b1Ptr, 1, kPtr, 1);
+			}
+		}
+	}
+
+
+
+
 	public static void Unary(int size, double f, double da, double daa, ReadOnlySpan<double> a, Span<double> r)
 	{
 		Debug.Assert(size >= 0);
